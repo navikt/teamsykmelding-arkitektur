3cd786df90862ce3d43ccb261245be148e982f1b
Karl J. Overå <k@rl.run>
2023-08-14 10:50:02 +0200

    pull when already cloned

--- src/git/cloner.ts
+++ src/git/cloner.ts
@@ -1,20 +1,25 @@
 import path from 'node:path'
 import * as fs from 'node:fs'
-import { git, gitOutputDir } from './git'
+import { cloner, createPuller, gitOutputDir } from './git'
 
 export async function cloneOrPull(repo: string): Promise<void> {
+    await (exists(repo) ? pull(repo) : clone(repo))
+}
+
+async function clone(repo: string) {
     const remote = `https://${Bun.env.GH_TOKEN}:x-oauth-basic@github.com/navikt/${repo}`
 
-    if (exists(repo)) {
-        git.pull(remote, undefined, {
-            '--rebase': null,
-        })
-        console.info(`${repo}, exists, pulled OK`)
-        return
-    }
-    await git.clone(remote, repo, { '--depth': 1 })
+    await cloner.clone(remote, repo, { '--depth': 1 })
 
     console.info(`Cloned ${repo} OK`)
+}
+
+async function pull(repo: string) {
+    await createPuller(repo).pull({
+        '--rebase': null,
+    })
+
+    console.info(`${repo}, exists, pulled OK`)
 }
 
 function exists(repo: string): boolean {
--- src/git/git.ts
+++ src/git/git.ts
@@ -1,15 +1,25 @@
 import * as fs from 'node:fs'
 import path from 'node:path'
-import simpleGit from 'simple-git'
+import simpleGit, { SimpleGit } from 'simple-git'
 
 export const gitOutputDir = path.join(process.cwd(), '.cache')
 
 if (!fs.existsSync(gitOutputDir)) {
-    fs.mkdirSync(gitOutputDir)
+    fs.mkdirSync(gitOutputDir, {
+        recursive: true,
+    })
 }
 
-export const git = simpleGit({
+export const cloner = simpleGit({
     baseDir: gitOutputDir,
     binary: 'git',
     maxConcurrentProcesses: 10,
 })
+
+export function createPuller(repo: string): SimpleGit {
+    return simpleGit({
+        baseDir: `${gitOutputDir}/${repo}`,
+        binary: 'git',
+        maxConcurrentProcesses: 1,
+    })
+}
