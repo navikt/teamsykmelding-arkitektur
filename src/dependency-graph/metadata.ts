b92001323b99825742c0a92c56c7abeffc64f214
Karl J. Overå <k@rl.run>
2023-08-15 14:47:00 +0200

    v1 of parsing, parses all repos

--- src/dependency-graph/metadata.ts
+++ src/dependency-graph/metadata.ts
@@ -3,15 +3,15 @@ import * as R from 'remeda'
 import { GithubWorkflowMetadata, Metadata, NaiseratorMetadata } from './types.ts'
 
 export function getApplicationName(metadata: Metadata[]): string | null {
-    const names = new Set(metadata.filter(naiseratorMetadata).map((nais) => nais.name))
+    const names = new Set(metadata.filter(naiseratorMetadata).map((nais) => nais.application))
     if (names.size === 0) {
-        console.log(
+        console.info(
             "Looks like this repo doesn't have any naiserator metadata, only github workflows, probably a naisjob",
         )
         return null
     } else if (names.size !== 1) {
         console.error('Wonky metadata:', metadata)
-        throw new Error(`Expected exactly one application name, got ${names.size}`)
+        throw new Error(`Expected exactly one application name, got ${names.size} (${Array.from(names).join(', ')})`)
     }
 
     return names.values().next().value
@@ -41,14 +41,15 @@ export function getMetadataForEnvironment(parsedMetadata: Metadata[], naiserator
     const metadata = parsedMetadata.filter(naiseratorMetadata).find((it) => it.file.includes(naiserator))
 
     if (!metadata) {
-        throw new Error(`Unable to find metadata for ${naiserator}`)
+        console.debug(`Seems like ${naiserator} is not a naiserator file, maybe some other nais resource?`)
+        return null
     }
 
     return metadata
 }
 
 export function naiseratorMetadata(metadata: Metadata): metadata is NaiseratorMetadata {
-    return 'name' in metadata
+    return 'file' in metadata
 }
 
 function githubWorkflowMetadata(metadata: Metadata): metadata is GithubWorkflowMetadata {
