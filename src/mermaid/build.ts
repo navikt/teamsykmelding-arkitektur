8c16465d2c0cef542b5d3ac26ecd9bedc59934ce
Karl J. Overå <k@rl.run>
2023-08-15 15:27:47 +0200

    add stub for mermaid generation

---
+++ src/mermaid/build.ts
@@ -0,0 +1,31 @@
+import * as R from 'remeda'
+
+import { DepedencyNodeMetadata, NodeDependencies } from '../dependency-graph/types.ts'
+
+export function buildMermaid(graph: DepedencyNodeMetadata[]): string {
+    const baseNodes = graph
+        .map((it) => it.application)
+        .slice(0, 5)
+        .join('\n')
+
+    const outboundConnections = graph.map((node) => createOutboundNodes(node.application, node.environments))
+
+    return `flowchart TB
+    ${baseNodes}
+    ${outboundConnections.join('\n')}
+`
+}
+
+function createOutboundNodes(application: string, environments: { env: string; dependencies: NodeDependencies }[]) {
+    return `
+        ${environments.map((env) => {
+            console.log('env', env.env)
+            return `
+                subgraph ${env.env}
+                ${env.dependencies.outbound.map((it) => `${application}--->${it.application}`).join('\n')}
+                ${env.dependencies.inbound.map((it) => `${application}<---${it.application}`).join('\n')}
+                end
+            `
+        })}
+    `
+}
