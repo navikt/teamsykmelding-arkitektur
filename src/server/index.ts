b5fa351d74593a86e37a3a8789215854e097549b
Karl J. Overå <k@rl.run>
2023-08-17 11:57:47 +0200

    add hot reload to index.html

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,12 @@
 import { type Serve } from 'bun'
 import path from 'node:path'
 import { buildDependencyGraph } from '../dependency-graph/build.ts'
+import { randomUUID } from 'crypto'
+
+const devModeUuid = Bun.env.NODE_ENV === 'production' ? null : randomUUID()
 
 export default {
-    async fetch(req) {
+    async fetch(req, server) {
         const url = new URL(req.url)
 
         if (url.pathname === '/favicon.ico') {
@@ -17,7 +20,7 @@ export default {
 
         if (url.pathname === '/data/graph-data.js') {
             const dependencyGraphData = await buildDependencyGraph({ cache: true })
-            return new Response(`window.tototot = ${JSON.stringify(dependencyGraphData)}`, {
+            return new Response(`window.graph = ${JSON.stringify(dependencyGraphData)}`, {
                 headers: {
                     'content-type': 'application/javascript',
                 },
@@ -33,11 +36,30 @@ export default {
         }
 
         if (url.pathname === '/') {
-            return new Response(await Bun.file(path.join(process.cwd(), 'public', 'index.html')).text(), {
+            const html = (await Bun.file(path.join(process.cwd(), 'public', 'index.html')).text()).replaceAll(
+                '${{ DYNAMIC_SCRIPTS }}',
+                Bun.env.NODE_ENV === 'production' ? '' : '<script src="/dev.js"></script>',
+            )
+
+            return new Response(html, {
                 headers: {
                     'content-type': 'text/html',
                 },
             })
+        }
+
+        if (Bun.env.NODE_ENV !== 'production') {
+            if (url.pathname === '/dev') {
+                if (server.upgrade(req)) {
+                    return
+                }
+
+                return new Response('This is a websocket endpoint', { status: 400 })
+            }
+
+            if (url.pathname === '/dev.js') {
+                return new Response(devModeBrowserScript, { headers: { 'content-type': 'application/javascript' } })
+            }
         }
 
         return new Response(`Fant ikke ressursen ${url.pathname}`, {
@@ -45,4 +67,52 @@ export default {
             headers: { contentType: 'text/plain' },
         })
     },
+    websocket: {
+        async message(ws, message) {
+            if (message === 'whoareyou') {
+                ws.send(devModeUuid ?? '')
+            }
+        },
+    },
 } satisfies Serve
+
+const devModeBrowserScript = /* language=JavaScript */ `let webSocket = null;
+let initialDevUUID = null;
+
+setInterval(() => {
+    webSocket.send('whoareyou')
+}, 1000)
+
+webSocket = webSockIt()
+
+function webSockIt() {
+    console.info("Setting up dev reload socket")
+
+    webSocket = new WebSocket('ws://localhost:3000/dev')
+    webSocket.onerror = () => {
+        setTimeout(() => {
+            webSocket = webSockIt()
+        }, 500)
+    }
+
+    webSocket.onclose = () => {
+        setTimeout(() => {
+            webSocket = webSockIt()
+        }, 500)
+    }
+    webSocket.onmessage = (event) => {
+        if (initialDevUUID == null) {
+            initialDevUUID = event.data
+            return;
+        }
+
+        console.log(initialDevUUID !== event.data, initialDevUUID, event.data)
+        if (initialDevUUID !== event.data) {
+            console.log('Hot reloading')
+            window.location.reload()
+        }
+    }
+
+    return webSocket;
+}
+`
